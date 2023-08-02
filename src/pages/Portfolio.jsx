66ab64da7dec01a040351a71823eb303e9c63f94
Kelly <kelly.rodrigues.oliveira@gmail.com>
2023-08-02 00:06:02 -0300

    Finalização do projeto

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,14 +1,44 @@
 import Header from "../components/Header"
-
+import styles from "../style/pages/portifolio.module.css"
 import portfolio from '../assets/portfolio.svg'
+import Axios from "axios"
+import { useEffect, useState } from "react"
 
 const Portfolio = () => {
+  const [repo, setRepos] = useState([])
+
+  useEffect(() => {
+    const getRepositorio = async () => {
+      try {
+        const response = await Axios.get(`https://api.github.com/users/Kelly-Oliveira/repos`)
+        setRepos(response.data)
+      } catch (err) {
+        console.error("Erro capturado: " + err)
+      }
+    }
+    getRepositorio()
+  }, []
+  )
+
+
   return (
     <>
       <Header
         title="Meus Projetinhos"
         image={portfolio} />
-        <h1>Portifólio</h1>
+
+      <div className={styles.repoContainer}>
+        {repo.map((repos) => {
+          return (
+            <div className={styles.cardRepo} key={repos.id}>
+              <h3 className={styles.cardTitle}>{repos.name}</h3>
+              <p className={styles.cardText}>{repos.description}</p>
+              <a className={styles.cardLink} href={repos.html_url} target="_blank"></a>
+            </div>
+          )
+        }
+        )}
+      </div>
     </>
   )
 }
--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,16 +1,36 @@
 import Header from "../components/Header"
-
 import sobre from '../assets/sobre.svg'
+import kelly from '../assets/kelly.jpg'
+import styles from '../style/pages/sobre.module.css'
 
 const Sobre = () => {
   return (
     <>
-      <Header 
-      title="Minha História"
-      image={sobre} />
-      <h1>Sobre</h1>
+      <Header
+        title="Minha História"
+        image={sobre} />
+
+
+      <div className={styles.sobreContainer}>
+        <div>
+          <img className={styles.fotoPerfil} src={kelly} alt="Foto da Kelly" />
+        </div>
+        <div className={styles.divParagraph}>
+          <p>Nasci em fevereiro de 1989, na infância já fiquei encantada com os computadores, quando terminei o ensino médio decidi entrar na faculdade de Análise de Sistemas.
+            Me senti perdida, um professor me aconselhou a fazer um curso técnico primeiro. Em abril de 2009 eu tive meu filho, Pedro. Em janeiro de 2010 eu estava começando a 
+            fazer o curso de Técnico em Redes de Computadores no SENAI/SP e após 24 meses estava formada.
+            Porém não conseguia emprego na área, a única maneira que conseguir de trabalhar na área onde eu não fosse julgada por ser mulher foi através do concurso público. 
+            Em 2013 eu passei no concurso do Hospital do Servidor
+            Público Municipal de São Paulo (HSPM) no cargo de Técnica de Informática (para suporte).</p>
+          <p>Eu me acomodei um pouco com o serviço público, mas depois de alguns anos entrei a Fatec São Paulo para estudar o curso Tecnológo de Redes de Computadores. 
+            Quando estava quase terminando o curso, me apaixonei pela área de front-end, faço cursos e estou cada dia mais apaixonada em ver a mágica acontecer.
+          </p>
+        </div>
+
+
+      </div>
     </>
-    
+
   )
 }
 
